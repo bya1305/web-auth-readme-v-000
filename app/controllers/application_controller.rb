f80c2b5ea4352b6da3e93dc58831c9916260c3a6
Josh Avina <josh.avina78@gmail.com>
2016-09-12 21:17:08 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+
+  private
+
+    def authenticate_user
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=SP44HEZDCZIU3UXYDB5X42JWJSU2HC4WEBQGRLAIGUCZX03C&response_type=code&redirect_uri=http://localhost:3000/auth"
+      redirect_to foursquare_url unless logged_in?
+    end
+
+    def logged_in?
+      !!session[:token]
+    end
 end
